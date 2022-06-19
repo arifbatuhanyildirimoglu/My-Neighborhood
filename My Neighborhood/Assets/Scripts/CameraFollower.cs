1e5ad87fe41eb746d7463f6f48a7613f8a9e09b4
arifbatuhanyildirimoglu <64323875+arifbatuhanyildirimoglu@users.noreply.github.com>
2022-06-20 02:49:18 +0300

    Update

--- My Neighborhood/Assets/Scripts/CameraFollower.cs
+++ My Neighborhood/Assets/Scripts/CameraFollower.cs
@@ -5,19 +5,18 @@ public class CameraFollower : MonoBehaviour
 {
     private Transform target;
     private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
         offset = transform.position - target.position;
     }
 
-    void LateUpdate(){
-        Vector3 newPosition = new Vector3(transform.position.x, target.position.y +8.8f, offset.z + target.position.z);
-        transform.DOMove(newPosition, 0.6f);
-    }
-
-    void Update()
-    {
+    void FixedUpdate(){
         
+        Vector3 newPosition = new Vector3(target.position.x, target.position.y + 14f, offset.z + target.position.z);
+        
+        transform.position = Vector3.SmoothDamp(transform.position,newPosition, ref velocity,0.2f);
     }
+    
 }
