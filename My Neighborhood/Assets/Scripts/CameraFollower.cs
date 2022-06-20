2c00d469faaea2de80f42d606932e61382106999
arifbatuhanyildirimoglu <64323875+arifbatuhanyildirimoglu@users.noreply.github.com>
2022-06-21 02:39:35 +0300

    Update

--- My Neighborhood/Assets/Scripts/CameraFollower.cs
+++ My Neighborhood/Assets/Scripts/CameraFollower.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollower : MonoBehaviour
 {
@@ -8,15 +9,31 @@ public class CameraFollower : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-        offset = transform.position - target.position;
+        if (SceneManager.GetActiveScene().name.Equals("Store"))
+        {
+            target = GameObject.FindGameObjectWithTag("Cart").transform;
+            offset = transform.position - target.position;
+        }else if (SceneManager.GetActiveScene().name.Equals("SampleScene"))
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+            offset = transform.position - target.position;    
+        }
+        
     }
 
     void FixedUpdate(){
         
-        Vector3 newPosition = new Vector3(target.position.x, target.position.y + 14f, offset.z + target.position.z);
+        Vector3 newPosition = new Vector3(target.position.x, target.position.y + offset.y, offset.z + target.position.z);
+
+        if (SceneManager.GetActiveScene().name.Equals("SampleScene"))
+        {
+            transform.position = Vector3.SmoothDamp(transform.position,newPosition, ref velocity,0.2f);
+        }
+        else
+        {
+            transform.position = newPosition;
+        }
         
-        transform.position = Vector3.SmoothDamp(transform.position,newPosition, ref velocity,0.2f);
     }
     
 }
--- My Neighborhood/Assets/Scripts/Grocery.cs
+++ My Neighborhood/Assets/Scripts/Grocery.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Grocery : Building
 {
@@ -10,12 +12,22 @@ public class Grocery : Building
     
     void Start()
     {
+        DontDestroyOnLoad(gameObject);
+        
         name = "Grocery";
         isOwned = true;
+
+        _stock = new Dictionary<Item, int>();
     }
 
     void Update()
     {
+        if (!SceneManager.GetActiveScene().name.Equals("SampleScene"))
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+        
         if (isOwned && !isMakingMoney)
         {
             isMakingMoney = true;
@@ -23,15 +35,20 @@ public class Grocery : Building
         } 
     }
 
-    private void AddToStock(Item item)
+    private void AddToStock(Item item, int amount)
     {
-        
+        _stock.Add(item,amount);
     }
 
     private void AddToStock(Dictionary<Item, int> items)
     {
-        
-        
+
+        foreach (Item item in items.Keys)
+        {
+            
+            _stock.Add(item, items[item]);
+            
+        }
         
     }
 
