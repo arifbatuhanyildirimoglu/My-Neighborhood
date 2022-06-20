2a95379296126cc204b4f79665d0abde919d5ff1
Ata Atak <ataatak90@gmail.com>
2022-06-20 04:24:09 +0300

    first commit

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class Building : MonoBehaviour
+{
+    protected string name;
+    protected int purchasePrice;
+    protected bool isOwned;
+    protected bool isMakingMoney;
+    protected float duration;
+    protected float amount;
+
+    protected IEnumerator MakeMoney(float duration, float amount)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(duration);
+            //Transform stackPosition = transform.GetChild(0);
+            Player.Instance.Budget += amount;
+            
+            //TODO: para çıkarıp ekranın sağ üstüne ekleme animasyonunu oynat
+        }
+    }
+
+    public string Name => name;
+    
+    public int PurchasePrice
+    {
+        get => purchasePrice;
+    }
+    
+    public bool IsOwned
+    {
+        get => isOwned;
+        set => isOwned = value;
+    }
+
+    public float Duration => duration;
+
+    public float Amount => amount;
+}
