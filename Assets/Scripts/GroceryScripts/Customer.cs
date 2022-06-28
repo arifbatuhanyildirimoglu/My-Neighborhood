e3d9da94ff53eea07e151d0f71b4de6fb64717e3
Ata Atak <ataatak90@gmail.com>
2022-06-29 02:57:06 +0300

    update

---
+++ Assets/Scripts/GroceryScripts/Customer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Customer : MonoBehaviour{
+    private NavMeshAgent navMeshAgent;
+    private GroceryManager groceryManager;
+    public bool IsCustomerGoingToGrocery{ get; set;}
+    private Animator animator;
+    
+    public Vector3 targetPosition;
+    void Start(){
+        IsCustomerGoingToGrocery = false;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        
+        
+    }
+
+    public void GoToTargetPosition(Vector3 targetPosition){
+        navMeshAgent.SetDestination(targetPosition);
+        
+        if (navMeshAgent.isStopped == false){
+            animator.SetBool("isWalking", true);
+        }
+        else{
+            animator.SetBool("isWalking", false);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/GroceryScripts/GroceryManager.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GroceryManager : MonoBehaviour
+{
+    [SerializeField]private GameObject customerModel;
+    [SerializeField]private Transform[] customerSpawnPositions;
+    [SerializeField]private Material[] customerMaterials;
+    [SerializeField]private bool isOpen;
+    [SerializeField]private bool isCustomerComing;
+    [SerializeField] private Transform customerPosition;
+    private GroceryManager instance;
+    private List<GameObject> customerList = new List<GameObject>();
+
+    void Awake(){
+        if (instance == null){
+            instance = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        isOpen = true;
+        isCustomerComing = false;
+        StartCoroutine(CreateDelayedCustomer());
+        StartCoroutine(SendDelayedCustomerToGrocery());
+        StartCoroutine(DestroyDelayedCustomer());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void CreateCustomer(){
+        int randomPositionNumber = Random.Range(0, 2);
+        GameObject currentCustomer = Instantiate(customerModel, customerSpawnPositions[randomPositionNumber].position, Quaternion.identity);
+        customerList.Add(currentCustomer);
+        
+        for (int i = 0; i < currentCustomer.transform.GetChild(0).childCount - 1; i++){
+            int randomNumber = Random.Range(0, 8);
+            currentCustomer.transform.GetChild(0).GetChild(i).GetComponent<SkinnedMeshRenderer>().material = customerMaterials[randomNumber];
+        }
+        
+        Customer customer = currentCustomer.GetComponent<Customer>();
+        Vector3 targetPosition = randomPositionNumber == 0 ? customerSpawnPositions[0].position : customerSpawnPositions[1].position;
+        Debug.Log(targetPosition);
+        customer.GoToTargetPosition(targetPosition);
+    }
+
+    IEnumerator CreateDelayedCustomer(){
+        while (true){
+            CreateCustomer();
+            Debug.Log("Created");
+            float randomDelayTime = Random.Range(3, 7);
+            yield return new WaitForSeconds(randomDelayTime); 
+        }
+    }
+
+    public void SendCustomerToGrocery(){
+        Customer customer = customerList[customerList.Count-1].GetComponent<Customer>();
+        customer.GoToTargetPosition(customerPosition.position);
+        isCustomerComing = true;
+        customer.IsCustomerGoingToGrocery = true;
+    }
+
+    IEnumerator SendDelayedCustomerToGrocery(){
+        while (isCustomerComing){
+            SendCustomerToGrocery();
+            yield return new WaitForSeconds(10); 
+        }
+    }
+
+    public void DestroyCustomer(){
+        for (int i = 0; i < customerList.Count; i++){
+            Customer customer = customerList[i].GetComponent<Customer>();
+            if (!customer.IsCustomerGoingToGrocery){
+                Destroy(customer.gameObject);
+                break;
+            }
+        }
+    }
+
+    IEnumerator DestroyDelayedCustomer(){
+        while (true){
+            DestroyCustomer();
+            yield return new WaitForSeconds(15); 
+        }
+    }
+}
